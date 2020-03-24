e18a24b06349d0d897c54e718702c00eaf9ddd7c
cvsgbhn <kbh123456g@gmail.com>
2020-03-24 23:57:08 +0300

    pam anderson is going to love me.

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -4,8 +4,7 @@
 
 /*
 ** Suggested algorithm:
-** Step 0. Read all given arguments and save it to void** array. Problem: count size of array before allocation
-**         Problem: adopt va_arg for int and char simultaneously
+** Step 0. Read all given arguments and save it to void** array.
 ** Step 1. Analyze all given arguments:
 ** Step 1.1. Analyze format string
 ** Step 1.2. While analyzing format string, if "%" - call function to detect what flag is this exactly
@@ -18,18 +17,12 @@
 int     ft_printf(const char *format, ...)
 {
     va_list argptr;
-    void    *narg;
-    //char    *first_arg;
     int     i;
 
     i = 0;
     va_start(argptr, format);
-    ft_putendl(va_arg(argptr, void*));
-    while((narg = va_arg(argptr, void*)))
-    {
+    while(va_arg(argptr, void*))
         i++;
-        ft_putendl((char*)narg);
-    }
     va_end(argptr);
     ft_putnbr(i);
     return (0);
