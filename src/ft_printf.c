c0c69873745a799ce5757ef4197e6e0a5ac14017
cvsgbhn <kbh123456g@gmail.com>
2020-05-04 16:36:26 +0300

    it works with a single %s

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,12 +6,13 @@
 
 char     *ft_putformat(const char **string)
 {
+    //ft_putendl("ft_printf.c ft_putformat");
     char    *traverse;
 
     traverse = (char *)*string;
     while( *traverse != '%' && *traverse)
     {
-        ft_putchar(*traverse);
+        //ft_putchar(*traverse);
         traverse++;
     }
     return traverse;
@@ -19,9 +20,7 @@ char     *ft_putformat(const char **string)
 
 int     ft_final_print(const char *fstr/*, va_list arg_list*/)
 {
-    //char    *position;
-
-    //position = ft_putformat(&fstr);
+    //ft_putendl("ft_printf.c ft_final_print");
     ft_putstr((char *)fstr);
     ft_putchar('\n');
     return (1);
@@ -29,28 +28,44 @@ int     ft_final_print(const char *fstr/*, va_list arg_list*/)
 
 int    ft_parse_args(t_pf *puts)
 {
+   // ft_putendl("ft_printf.c ft_parse_args");
     char    *position;
 
-    position = (char *)puts->format;
-    if(*position == 's')
-        ft_handle_string(puts);
+    position = ft_putformat((const char **)&(puts->format));
+    while (*position != '\0')
+    {
+        position++;
+        if (*position == 's')
+            ft_handle_string(puts);
+    }
     return (1);
 }
 
 int     ft_printf(const char *format, ...)
 {
+    //ft_putendl("ft_printf.c ft_printf");
     t_pf    *puts;
 
-    puts = (t_pf *)malloc(sizeof(t_pf));
+    if (!(puts = (t_pf *)malloc(sizeof(t_pf))))
+        return (-1);
+   // ft_putendl("1 ft_printf.c ft_printf");
     if(format)
     {
+     //   ft_putendl("2 ft_printf.c ft_printf");
         va_start(puts->args, format);
+       // ft_putendl("2.1 ft_printf.c ft_printf");
         puts->format = (char *)format;
+        //ft_putendl("2.2 ft_printf.c ft_printf");
+        puts->copy_format = ft_memalloc(ft_strlen(format));
         ft_strcpy(puts->copy_format, puts->format);
+       // ft_putendl("3 ft_printf.c ft_printf");
         if ((ft_parse_args(puts)) == 1)
             ft_final_print(puts->format/*, puts->args*/);
+        //ft_putendl("4 ft_printf.c ft_printf");
         va_end(puts->args);
+        //ft_putendl("4.1 ft_printf.c ft_printf");
     }
-    ft_memdel((void **)puts);
+    free(puts);
+    //ft_putendl("5 ft_printf.c ft_printf");
     return (0);
 }
--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    ft_printf("Hello, World!\n%s", "I love you!");
+    ft_printf("Hello, World!\n%s\n%s", "I love you!", "~UwU~");
     //ft_printf("Hello\n", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     //ft_printf("Bye\n", 'a', 'b', 'c', "fgweth");
     return 0;
